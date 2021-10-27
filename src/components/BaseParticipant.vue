c2e929a2bf4ca63b0a0e8d3d2bcd7df452067a78
Connor Forbes <connorforbes25@gmail.com>
2021-10-27 15:47:20 +1000

    init: Base setup

---
+++ src/components/BaseParticipant.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<p>Participant</p>
+		<p>{{color}}</p>
+		<p>{{isCluster}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	props: {
+		color: String,
+		isCluster: Boolean
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
